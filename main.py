368a82018cec30b01591886d753271d770414985
ryleu <69326171+ryleu@users.noreply.github.com>
2024-08-03 00:33:15 -0400

    switch to songlink cuz songwhip is dead

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 import interactions
 import re
 import typing
+from urllib.parse import quote
 
 if os.path.exists("config.json"):
     with open("config.json") as file:
@@ -24,49 +25,63 @@ url_regex = re.compile(
 
 def get_song_data(url: str) -> interactions.Embed:
     # get the data from the songwhip api
-    response: requests.Response = requests.post(
-        url="https://songwhip.com", data=json.dumps({"url": url})
+    response: requests.Response = requests.get(
+        url= f"https://api.song.link/v1-alpha.1/links?url={quote(url)}&userCountry=US"
     )
     data: dict = response.json()
 
     # check and make sure we have a valid response
-    data["status"] = data.get("status", "ok")
-
-    if data["status"] == "error":
+    if response.status_code not in range(200, 300):
         return interactions.Embed(
             title="Error",
-            description=data["error"]["message"],
+            description=data,
             color=0xFF0000,
         )
-    elif data["status"] != "ok":
-        return interactions.Embed(
-            title="Unknown status",
-            description=json.dumps(data),
-            color=0xFF0000,
-        )
+
+    # parse the data we care about
+    title = None
+    artist = None
+    thumbnail = None
+
+    relevant_platforms = [
+        "spotify",
+        "youtubeMusic",
+        "appleMusic",
+        "amazonMusic",
+    ]
+    relevant_data = []
+    for platform in relevant_platforms:
+        by_platform = data["linksByPlatform"].get(platform, None)
+        if by_platform == None:
+            continue
+        url = by_platform["url"]
+        id = by_platform["entityUniqueId"]
+        entity = data["entitiesByUniqueId"][id]
+        if title == None: title = entity.get("title", None)
+        if artist == None: artist = entity.get("artistName", None)
+        if thumbnail == None: thumbnail = entity.get("thumbnailUrl", None)
+
+        relevant_data.append({
+            "platform": platform,
+            "url": url,
+        })
 
     # return the embed
     return interactions.Embed(
-        title=data["name"],
-        description="**By:** "
-        + "".join(
-            # create a list of artist names to combine into a string
-            ["\n- " + artist["name"] for artist in data["artists"]]
-        )
-        + f"\n\n**Type:** {data['type']}",
+        title=f"{title} by {artist}",
+        description="listen on:\n" + "\n".join([
+            f"- [{x["platform"]}]({x["url"]})" for x in relevant_data
+        ]),
         color=0x00FFFF,
-        url=data["url"],
-        thumbnail=data["image"],
+        url=data["pageUrl"],
+        thumbnail=thumbnail,
     )
 
 
-def author_branding() -> typing.Optional[interactions.EmbedAuthor]:
-    if config["disable_branding"]:
-        return None
+def author_branding() -> interactions.EmbedAuthor:
     return interactions.EmbedAuthor(
-        name="Help pay for server costs!",
-        icon_url="https://cdn.discordapp.com/attachments/810959625306243095/1160059628281929810/kofi_s_logo_nolabel.png",
-        url="https://ko-fi.com/ryleu",
+        name="Powered by Odesli.",
+        url="https://odesli.co/",
     )
 
 
@@ -116,6 +131,6 @@ async def get_song_data_from_message(ctx: interactions.ContextMenuContext):
 
     await ctx.respond(content="Found these song(s):", embeds=embeds, ephemeral=True)
 
-
-# run the bot
-bot.start(token=config["bot_token"])
+if __name__ == "__main__":
+    # run the bot
+    bot.start(token=config["bot_token"])
